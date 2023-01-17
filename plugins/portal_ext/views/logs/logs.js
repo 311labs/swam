d5c4377e533873b18242e816c4df6237568640c1
Ian Starnes <ians@311labs.com>
2023-01-17 09:26:05 -0800

    ability to set log date range

--- plugins/portal_ext/views/logs/logs.js
+++ plugins/portal_ext/views/logs/logs.js
@@ -3,7 +3,8 @@
 PORTAL.Views.Logs = SWAM.Views.AdvancedTable.extend({
     classes: "swam-paginated-table swam-table-clickable swam-table-tiny",
     defaults: {
-        model_field: "user",
+        param_field: "user",
+        model_field: "id",
         columns: [
             {label:"Created", field:"when|datetime"},
             {label:"Who", field:"model.user.username|ifempty(model.tid)", classes:"d-none d-xl-table-cell"},
@@ -149,24 +150,39 @@ PORTAL.Views.Logs = SWAM.Views.AdvancedTable.extend({
 
     setModel: function(model) {
         this.model = model;
-        if (this.options.model_field == "user") {
+        if (this.options.model_field && this.options.param_field) {
+            this.collection.params[this.options.param_field] = model.get(this.options.model_field);
+        } else if (this.options.param_field == "user") {
             if (model.get("member")) {
                 this.collection.params.user = model.get("member.id");
             } else {
                 this.collection.params.user = model.id;
             }
-        } else if (this.options.model_field == "terminal") {
+        } else if (this.options.param_field == "terminal") {
             this.collection.params.tid = model.get("tid");
-        } else if (this.options.model_field) {
-            this.collection.params[this.options.model_field] = model.id;
+        } else if (this.options.param_field) {
+            this.collection.params[this.options.param_field] = model.id;
         }
 
         if (this.options.component) {
             this.collection.params.component = this.options.component;
-            this.collection.params.pkey = model.id;
+            if (this.options.component_field) {
+                this.collection.params.pkey = model.get(this.options.model_field);
+            } else {
+                this.collection.params.pkey = model.id;
+            }
+            
         }
 
-        
+        if (this.options.date_range && this.options.date_range.dr_start_field) {
+            this.collection.params.dr_start = model.get(this.options.date_range.dr_start_field || "created");
+            this.collection.params.dr_end = model.get(this.options.date_range.dr_end || "modified");
+            if (this.options.date_range.padding) {
+                this.collection.params.dr_start -= this.options.date_range.padding;
+                this.collection.params.dr_end += this.options.date_range.padding;
+            }
+        }
+
         if (app.group) {
             this.collection.params.group = app.group.id;
         } else if (this.collection.params.group) {
