c29880700e1652e8087fc441c7e59610e3223e46
Ian Starnes <ians@311labs.com>
2023-03-21 13:12:48 -0700

    default admin menu

--- plugins/portal_ext/namespace.js
+++ plugins/portal_ext/namespace.js
@@ -1,4 +1,5 @@
 window.PORTAL = window.PORTAL || {};
+window.PORTAL.Menus = window.PORTAL.Menus || {};
 window.PORTAL.Views = window.PORTAL.Views || {};
 window.PORTAL.Pages = window.PORTAL.Pages || {};
 
@@ -21,3 +22,121 @@ window.PORTAL.init_admin_pages = function() {
 	app.addPage("incident_events", new PORTAL.Pages.IncidentEvents({group_filtering:false}), ["admin/incident/events"]);
 	app.addPage("incident_rules", new PORTAL.Pages.IncidentRules({group_filtering:false}), ["admin/incident/rules"]);
 }
+
+PORTAL.Menus.Admin = [
+	{
+		label: "Dashboard",
+		icon: "speedometer2",
+		page: "admin_dashboard"
+	},
+	{
+		icon: "wrench-adjustable-circle",
+		label: "System",
+		items: [
+			{
+				icon: "person-circle",
+				label:"Users",
+				page: "users"
+			},
+			{
+				icon: "people-fill",
+				label:"Groups",
+				page: "groups"
+			},
+			{
+				icon: "journals",
+				label:"Logs",
+				page: "audit_logs"
+			},
+			{
+				icon: "list-check",
+				label:"Task Queue",
+				page: "taskqueue"
+			}
+		]
+	},
+	{
+		icon: "mailbox",
+		label: "Email",
+		items: [
+			{
+				icon: "mailbox",
+				label:"Inbox",
+				page: "email_inbox"
+			},
+			{
+				icon: "envelope-open",
+				label:"Incoming",
+				page: "email_message"
+			},
+			{
+				icon: "send",
+				label:"Outgoing",
+				page: "email_outbox"
+			},
+			{
+				icon: "envelope-slash-fill",
+				label:"Bounced",
+				page: "email_bounced"
+			},
+			{
+				icon: "envelope-exclamation-fill",
+				label:"Complaints",
+				page: "email_complaint"
+			},
+		]
+	},
+	{
+		icon: "mailbox",
+		label: "Incidents",
+		items: [
+			{
+				icon: "exclamation-diamond-fill",
+				label:"Incidents",
+				page: "incidents"
+			},
+			{
+				icon: "exclamation-circle-fill",
+				label:"Events",
+				page: "incident_events"
+			},
+			{
+				icon: "list-check",
+				label:"Rules",
+				page: "incident_rules"
+			}
+		]
+	},
+	{
+		icon: "wrench-adjustable-circle-fill",
+		label:"Global",
+		page: "admin_global"
+	},
+	{
+		icon: "file-earmark-image",
+		label:"Media",
+		page: "admin_media"
+	},
+	{
+		icon: "book",
+		label: "Developer Help",
+		items: [
+			{
+				label:"UI Docs",
+				icon: "book",
+				url: "/docs/developer"
+			},
+			{
+				label:"Bootstrap Docs",
+				icon: "bootstrap-fill",
+				url: "https://getbootstrap.com/docs/5.2/getting-started/introduction/"
+			},
+			{
+				label:"Bootstrap Icons",
+				icon: "bootstrap-fill",
+				url: "https://icons.getbootstrap.com/"
+			},
+		]
+	},
+
+]
