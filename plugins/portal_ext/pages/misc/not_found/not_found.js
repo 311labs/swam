6edac64be43e0d12092ddefa147a8cd61e4de52d
Ian Starnes <ians@311labs.com>
2024-01-06 14:42:57 -0800

    fullscreen

--- plugins/portal_ext/pages/misc/not_found/not_found.js
+++ plugins/portal_ext/pages/misc/not_found/not_found.js
@@ -1,12 +1,12 @@
 
 PORTAL.Pages.NotFound = SWAM.Page.extend({
     template: "portal_ext.pages.misc.not_found",
-    classes: "has-topbar"
+    classes: "has-topbar page-view page-fullscreen"
 
 });
 
 PORTAL.Pages.Denied = SWAM.Page.extend({
     template: "portal_ext.pages.misc.not_found.denied",
-    classes: "has-topbar"
+    classes: "has-topbar page-view page-fullscreen bg-white"
 
 });
