cf7257cc0e05abed93975953ef5c9e7010858bd7
Ian Starnes <ians@311labs.com>
2024-03-08 18:29:15 -0800

    more abstract chat widget

--- plugins/portal_ext/pages/admin/incidents/incidents/incident.js
+++ plugins/portal_ext/pages/admin/incidents/incidents/incident.js
@@ -1,5 +1,152 @@
+PORTAL.Views.Incident = SWAM.View.extend({
+    template: "portal_ext.pages.admin.incidents.incidents.view",
+    classes: "swam-view h-100",
 
-PORTAL.Views.Incident = SWAM.Views.Tabs.extend({
+    on_init: function() {
+        this.addChild("tabs", new PORTAL.Views.IncidentTabs());
+        this.addChild("notes", new SWAM.Views.ChatView({
+            item_options: {
+                message_field: "note"
+            },
+            collection: new SWAM.Collections.IncidentHistory()
+        }));
+
+        this.notes = this.getChild("notes");
+        this.notes.on("new_msg", this.on_new_msg, this);
+    },
+
+    setModel: function(model) {
+        // filter the collection models to pertain only to the views model id
+        SWAM.View.prototype.setModel.call(this, model);
+        this.options.model = model;
+        this.getTab("events").collection.params.incident = this.options.model.get("id");
+        this.getTab("events").collection.reset();
+        // this.getTab("history").collection.params.parent = this.options.model.get("id");
+        // this.getTab("history").collection.reset();
+        this.notes.collection.params.parent = this.options.model.get("id");
+        this.notes.collection.fetch()
+    },
+
+    on_new_msg: function(evt) {
+        if (!evt.value) return;
+        this.notes.showBusy();
+        let model = new PAYOMI.Models.FFOrderNote();
+        let data = {text:evt.value};
+        data.by = app.me.id;
+        data.group = this.model.get("group.id");
+        data.order = this.model.id;
+        model.save(data, function(resp){
+            this.notes.hideBusy();
+            this.notes.clearMessage();
+            this.notes.collection.fetch();
+        }.bind(this));
+    },
+
+    getTab: function(tab) {
+        return this.children.tabs.getTab(tab);
+    },
+
+
+    showDialog: function(model, collection, parent) {
+        let state = model.get("state");
+        let state_display = model.get("state_display");
+        let description = model.get("description");
+        let component = model.get("component"); 
+        let item = {model:model};
+        let context_menu = [
+            {
+                label: "Edit Incident",
+                icon: "pencil",
+                callback: function(dlg, menu) {
+                    parent.on_item_edit(item);
+                }
+            }
+        ];
+
+        this.setModel(model);
+
+        if (model.get("rule")) {
+            context_menu.push({
+                    label: "Edit Rule",
+                    icon: "pencil",
+                    callback: function(dlg, menu) {
+                        parent.on_rule_edit(item);
+                    }
+                });
+        }
+
+        context_menu.push({divider:true});
+        let menu_state = {};
+        if (state < 3) {
+            if (state == 0) {
+                context_menu.push({
+                    label: "Open",
+                    icon: "shield-check",
+                    callback: function(dlg, menu) {
+                        app.showBusy();
+                        model.save({state:1}, function(){
+                            app.hideBusy();
+                            collection.fetch();
+                        });
+                    }
+                });
+            } else if (state == 1) {
+                context_menu.push({
+                    label: "Pause",
+                    icon: "pause-circle",
+                    callback: function(dlg, menu) {
+                        app.showBusy();
+                        model.save({state:2}, function(){
+                            app.hideBusy();
+                            dlg.dismiss();
+                            collection.fetch();
+                        });
+                    }
+                });
+            }
+
+            context_menu.push({
+                label: "Ignore",
+                icon: "shield-slash-fill",
+                callback: function(dlg, menu) {
+                    app.showBusy();
+                    model.save({state:3}, function(){
+                        app.hideBusy();
+                        dlg.dismiss();
+                        collection.fetch();
+                    });
+                }
+            });
+
+            context_menu.push({
+                label: "Resolved",
+                icon: "shield-fill-check",
+                callback: function(dlg, menu) {
+                    app.showBusy();
+                    model.save({state:4}, function(){
+                        app.hideBusy();
+                        dlg.dismiss();
+                        collection.fetch();
+                    });
+                }
+            });
+        }
+        let header = SWAM.renderTemplate("portal_ext.pages.admin.incidents.incidents.header", {model:model});
+        return SWAM.Dialog.showView(this, {
+            title: header,
+            kind: "primary",
+            can_dismiss: true,
+            padded: true,
+            scrollable: true,
+            fullscreen: true,
+            // size: 'lg',
+            // height: 'md',
+            "context_menu": context_menu
+        });
+    }
+
+});
+PORTAL.Views.IncidentTabs = SWAM.Views.Tabs.extend({
     on_init: function() {
         SWAM.Views.Tabs.prototype.on_init.call(this);
 
@@ -97,35 +244,25 @@ PORTAL.Views.Incident = SWAM.Views.Tabs.extend({
     },
 
     init_history_tab: function() {
-        let collection = new SWAM.Collections.IncidentHistory();
-        collection.params.parent = -1;
-        this.addTab("Notes", "history", new SWAM.Views.AdvancedTable({
-            collection: collection,
-            remote_sort: false,
-            add_button: {
-                type: "button",
-                action: "add",
-                label: "<i class='bi bi-pencil'></i> Add Note",
-                classes: "btn btn-primary",
-                columns:3
-            },
-            add_classes: "swam-table-clickable",
-            columns: [
-                {label:"When", template:"<div>{{model.by.display_name}}</div><div>{{model.created|datetime}}</div>"},
-                {label:"Note", template:"<div>{{{model.note}}}</div>", td_classes:"td-pre"}
-            ],
-            pagination: true,
-        }));
-    },
-
-    setModel: function(model) {
-        // filter the collection models to pertain only to the views model id
-        SWAM.Views.Tabs.prototype.setModel.call(this, model);
-        this.options.model = model;
-        this.getTab("events").collection.params.incident = this.options.model.get("id");
-        this.getTab("events").collection.reset();
-        this.getTab("history").collection.params.parent = this.options.model.get("id");
-        this.getTab("history").collection.reset();
+        // let collection = new SWAM.Collections.IncidentHistory();
+        // collection.params.parent = -1;
+        // this.addTab("Notes", "history", new SWAM.Views.AdvancedTable({
+        //     collection: collection,
+        //     remote_sort: false,
+        //     add_button: {
+        //         type: "button",
+        //         action: "add",
+        //         label: "<i class='bi bi-pencil'></i> Add Note",
+        //         classes: "btn btn-primary",
+        //         columns:3
+        //     },
+        //     add_classes: "swam-table-clickable",
+        //     columns: [
+        //         {label:"When", template:"<div>{{model.by.display_name}}</div><div>{{model.created|datetime}}</div>"},
+        //         {label:"Note", template:"<div>{{{model.note}}}</div>", td_classes:"td-pre"}
+        //     ],
+        //     pagination: true,
+        // }));
     },
 
     on_action_add: function(evt) {
@@ -164,100 +301,4 @@ PORTAL.Views.Incident = SWAM.Views.Tabs.extend({
         SWAM.Dialog.editModel(new view.collection.options.Model(), options);
     },
 
-    showDialog: function(model, collection, parent) {
-        let state = model.get("state");
-        let state_display = model.get("state_display");
-        let description = model.get("description");
-        let component = model.get("component"); 
-        let item = {model:model};
-        let context_menu = [
-            {
-                label: "Edit Incident",
-                icon: "pencil",
-                callback: function(dlg, menu) {
-                    parent.on_item_edit(item);
-                }
-            }
-        ];
-
-        this.setModel(model);
-
-        if (model.get("rule")) {
-            context_menu.push({
-                    label: "Edit Rule",
-                    icon: "pencil",
-                    callback: function(dlg, menu) {
-                        parent.on_rule_edit(item);
-                    }
-                });
-        }
-
-        context_menu.push({divider:true});
-        let menu_state = {};
-        if (state < 3) {
-            if (state == 0) {
-                context_menu.push({
-                    label: "Open",
-                    icon: "shield-check",
-                    callback: function(dlg, menu) {
-                        app.showBusy();
-                        model.save({state:1}, function(){
-                            app.hideBusy();
-                            collection.fetch();
-                        });
-                    }
-                });
-            } else if (state == 1) {
-                context_menu.push({
-                    label: "Pause",
-                    icon: "pause-circle",
-                    callback: function(dlg, menu) {
-                        app.showBusy();
-                        model.save({state:2}, function(){
-                            app.hideBusy();
-                            dlg.dismiss();
-                            collection.fetch();
-                        });
-                    }
-                });
-            }
-
-            context_menu.push({
-                label: "Ignore",
-                icon: "shield-slash-fill",
-                callback: function(dlg, menu) {
-                    app.showBusy();
-                    model.save({state:3}, function(){
-                        app.hideBusy();
-                        dlg.dismiss();
-                        collection.fetch();
-                    });
-                }
-            });
-
-            context_menu.push({
-                label: "Resolved",
-                icon: "shield-fill-check",
-                callback: function(dlg, menu) {
-                    app.showBusy();
-                    model.save({state:4}, function(){
-                        app.hideBusy();
-                        dlg.dismiss();
-                        collection.fetch();
-                    });
-                }
-            });
-        }
-        let header = SWAM.renderTemplate("portal_ext.pages.admin.incidents.incidents.header", {model:model});
-        return SWAM.Dialog.showView(this, {
-            title: header,
-            kind: "primary",
-            can_dismiss: true,
-            padded: true,
-            scrollable: true,
-            size: 'lg',
-            height: 'md',
-            "context_menu": context_menu
-        });
-    }
 });
